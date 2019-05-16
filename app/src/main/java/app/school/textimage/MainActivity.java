b90b6285153b621a8ecd7b820b7f950a6674483c
jogilsang <jogilsang@gmail.com>
2019-05-16 15:56:12 +0900

    update

---
+++ app/src/main/java/app/school/textimage/MainActivity.java
@@ -0,0 +1,29 @@
+package app.school.textimage;
+
+import android.os.Bundle;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        initView();
+        setListener();
+    }
+
+
+    @Override
+    public void initView() {
+
+
+    }
+
+    @Override
+    public void setListener(){
+
+
+    }
+
+}
